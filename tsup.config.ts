b811bbb29272371bda036b5d9522c85bfb7b0a3d
Darian Bailey <darian@nftychat.xyz>
2024-11-09 00:14:16 -0500

    make filenames match

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".js",
+    };
+  },
 });
